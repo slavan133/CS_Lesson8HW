b7a6e59b5a27b0afad1af65e59582d2e0aaf157c
Vyacheslav Nosov <slavan133@yandex.ru>
2023-06-20 20:14:26 +0300

    Hometask done

--- Task58HW/Program.cs
+++ Task58HW/Program.cs
@@ -6,16 +6,6 @@
 // 18 20
 // 15 18
 
-int[,] mult(int[,] ar1, int[,] ar2)
-        {
-            int[,] m = new int[ar1.GetLength(0), ar2.GetLength(1)];
- 
-            for (int i = 0; i < ar1.GetLength(0); ++i)
-                for (int j = 0; j < ar2.GetLength(0); ++j)
-                    for (int k = 0; k < ar2.GetLength(1); ++k)
-                        m[i, k] += ar1[i, j] * ar2[j, k];
-            return m;
-        }
 
 int Prompt(string text)
 {
--- Task62HW/Program.cs
+++ Task62HW/Program.cs
@@ -4,3 +4,51 @@
 // 12 13 14 05
 // 11 16 15 06
 // 10 09 08 07
+
+
+int [,] SpiralArray4x4()
+{
+int n = 4;
+int[,] spiraleMatrix = new int[n, n];
+int temp = 1;
+int i = 0;
+int j = 0;
+
+while (temp <= spiraleMatrix.GetLength(0) * spiraleMatrix.GetLength(1))
+{
+  spiraleMatrix[i, j] = temp;
+  temp++;
+  if (i <= j + 1 && i + j < 3)
+    j++;
+  else if (i < j && i + j >= 3)
+    i++;
+  else if (i >= j && i + j > 3)
+    j--;
+  else
+    i--;
+}
+return spiraleMatrix;
+}
+
+void PrintArraySpiral(int[,] array2d)
+{
+    for (int i = 0; i < array2d.GetLength(0); i++)
+    {
+        for (int j = 0; j < array2d.GetLength(1); j++)
+        {
+            if (array2d [i,j] < 10)
+            {
+              Console.Write($" {array2d[i,j]} ");  
+            } 
+            else
+            {
+              Console.Write($"{array2d[i,j]} ");  
+            }
+        }
+        Console.WriteLine();
+    }
+}
+
+int [,] arrayspiral = SpiralArray4x4();
+Console.WriteLine("Спиральный массив 4 на 4:");
+PrintArraySpiral(arrayspiral);
